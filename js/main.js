cc6947c2c227a09540f4da598d5c19b868307477
Nathalia Lima <84218599+NathaliaLima-Front@users.noreply.github.com>
2024-02-29 12:47:51 -0500

    Bugs fixed
    In this commit, the bugs mentioned in the readme were fixed and some improvements were included, such as a lightbox for the solved puzzle, for example. The images were also added.

--- js/main.js
+++ js/main.js
@@ -1,67 +1,78 @@
-// variables always go at the top -> this is step 1
-// these are the connections that you're making to elements on the page 
-// use CSS selectors to make connections to elements with JavaScript
-
-// create a 1 to 1 connection with a variable -> querySelector("queryString")
-// let theButton = document.querySelector("#buttonOne");
-
-// create a 1 to many connection with a variable -> querySelectorAll("queryString")
-let theButtons = document.querySelectorAll("#buttonHolder img"),
+let resetButton = document.querySelectorAll("#resetBut"),
+	theButtons = document.querySelectorAll("#buttonHolder img"),
 	puzzleBoard = document.querySelector(".puzzle-board"),
 	puzzleSpots = document.querySelectorAll(".puzzle-board div"),
-	puzzlePieces = document.querySelectorAll(".puzzle-pieces img"),
+	puzzlePieces = document.querySelector(".puzzle-pieces"),
+	puzzlePiecesImg = document.querySelectorAll(".puzzle-pieces img"),
 	dropZones = document.querySelectorAll('.drop-zone'),
-	// could be both querySelectorAll or child properties, both would work
-	// store the dragged piece in a global variable
-	// because we need it in the handleDrop function
+	sameSpot = document.querySelector('#same_spot'),
+	allPiecesCorrect = document.querySelector('#all_pieces_correct'),
 	draggedPiece;
 
-	// step 3
-// functionality always goes in the middle -> how do we want
-// the app to behave?
 function changeBGImage() {
-	// the `` is a JavaScript template string. It tells the JS enging to evaluate the expression
-	// inside the braces - run that little bit of code. In this case it's just pulling the ID of the
-	// button we clicked on and putting it at the end of the image name (0, 1, 2, 3)
-	// and updating the background-image style of the puzzle board element.
-
-	// bug fix #2 should go here. it's at most 3 lines of JS code.
-
 	puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`;
+
+	puzzlePiecesImg.forEach(piece => {
+		const jpgPosition = piece.src.indexOf(".jpg");
+		const pieceName = piece.src.slice(0, jpgPosition - 1);
+		const newName = pieceName + this.id + ".jpg";
+		piece.src = newName;
+    });
+
+	reset();
+}
+
+function reset() {
+    puzzlePiecesImg.forEach(piece => {
+        puzzlePieces.appendChild(piece);
+    });
 }
 
 function handleStartDrag() {
-	console.log('started dragging this piece:', this);
-	// store a reference to the puzzle piece image that we're dragging
-	// so we can use it later and move it to a drop zone
 	draggedPiece = this;
 }
 
 function handleDragOver(event) {
 	event.preventDefault();
-	console.log('dragged over dropzone!');
 };
 
 function handleDrop(event) {
 	event.preventDefault();
-	console.log('dropped something on dropzone!');
-	// bug fix #1 should go here, and it's at most 3 lines of JS code >.>
+	if (this.children.length == 0) {
+		this.appendChild(draggedPiece);
+
+		if (puzzlePieces.children.length === 0) {
+            let allCorrect = true;
+            puzzleSpots.forEach(spot => {
+                if (spot.children.length === 1) {
+                    const pieceSrc = spot.children[0].src;
+                    if (pieceSrc.indexOf(spot.id) === -1) {
+                        allCorrect = false;
+                    }
+                } else {
+                    allCorrect = false;
+                }
+            });
+
+            if (allCorrect) {
+				allPiecesCorrect.style.opacity = 1;
+				setTimeout(() => {
+					allPiecesCorrect.style.opacity = 0;
+				}, 7000);
+            }
+        }
+	} else {
+		sameSpot.style.opacity = 1;
+		setTimeout(() => {
+			sameSpot.style.opacity = 0;
+		}, 3000);
+	}
 };
 
-
-// step 2
-// event handling always goes at the bottom =>
-// how do we want users to interact with our app
-
-// 1 to 1 event handling
-//theButton.addEventListener("click", changeBGImage);
-
-// 1 to many event handling
-// add event handling to each button in the collection of buttons, one at a time
+resetButton.forEach(button => button.addEventListener("click", reset));
 theButtons.forEach(button => button.addEventListener("click", changeBGImage));
 
-// add the drag event handling to the puzzle pieces
-puzzlePieces.forEach(piece => piece.addEventListener("dragstart", handleStartDrag));
+puzzlePiecesImg.forEach(piece => piece.addEventListener("dragstart", handleStartDrag));
 
 dropZones.forEach(zone => zone.addEventListener('dragover', handleDragOver));
 dropZones.forEach(zone => zone.addEventListener('drop', handleDrop));
